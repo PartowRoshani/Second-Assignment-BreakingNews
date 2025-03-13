b1da89dbd18c0684e8bdd61aeb70e59e02e33dce
Partow Roshani <roshanipartow@gmail.com>
2025-03-13 13:19:08 +0330

    Seprating News class file

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -14,41 +14,12 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import ir.huri.jcal.JalaliCalendar;
 
 
-
-class News
-{
-    final private String title;
-    final private String description;
-    final private String sourceName;
-    final private String author;
-    final private String url;
-    final private String publishedAt;
-    public News(String title , String description , String sourceName , String author , String url , String publishedAt)
-    {
-        this.title = title;
-        this.description = description;
-        this.sourceName = sourceName;
-        this.author = author;
-        this.url = url;
-        this.publishedAt = publishedAt;
-    }
-    public void displayNews()
-    {
-        System.out.println("title : " + title);
-        System.out.println("Description : "+ description);
-        System.out.println("Source Name : "+sourceName);
-        System.out.println("Author : "+ author);
-        System.out.println("Url : "+ url);
-        System.out.println("Published At : " + publishedAt);
-
-    }
-
-    public String getTitle() {
-        return title;
-    }
-}
+import AP.News;
 
 public class Infrastructure {
 
--- BreakingNews/src/main/java/AP/Main.java
+++ BreakingNews/src/main/java/AP/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Infrastructure newsApp = new Infrastructure("980c8f4f4a934c799e5ebde0732d1cf3");
         newsApp.displayNewsList();
+        scanner.close();
     }
 }
